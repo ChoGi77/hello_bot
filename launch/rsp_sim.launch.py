5ad971e123874704c924947ddf5e472dbe47b0f3
ChoGi77 <chrlghkdydrpwjd@gmail.com>
2025-07-12 08:36:58 +0900

    Add simulated lidar

--- launch/rsp_sim.launch.py
+++ launch/rsp_sim.launch.py
@@ -26,10 +26,15 @@ def generate_launch_description():
     )
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
+    world_path = os.path.join(
+        get_package_share_directory(package_name), 'config', 'hello_world')
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
+        ]),
+        launch_arguments={'world': world_path}.items()
+    )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
